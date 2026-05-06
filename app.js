b24bfd6bbb3830d41a846fea2740e7877f6a9b66
ujjwal <ujjwal@gmail.com>
2026-05-07 03:19:43 +0530

    Update in my file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps Project 1 Running 🚀');
+    res.send('DevOps Project 1');
 });
 
 app.listen(3000, () => {
