0c1d6e12ed18e99cd556d99121ac2d593219f637
ujjwalsinghal05 <ujjwal.singhal05@gmail.com>
2026-05-17 10:04:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('DevOps Project automate by CI/CD pipeline');
+    res.send('DevOps Project automate by CI/CD pipelines');
 });
 
 app.listen(3000, () => {
